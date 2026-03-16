04346d55b3375faac0c8581864678b115f5e44ef
davehenzy <davehenzy@gmail.com>
2026-03-16 03:30:52 +0100

    2 admin

--- tmp_schema.php
+++ tmp_schema.php
@@ -1,5 +1,15 @@
 <?php
-require_once 'c:/xampp/htdocs/swiftcap/includes/db.php';
-$stmt = $pdo->query("DESCRIBE users");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
+require_once 'includes/db.php';
+
+echo "--- TABLES ---\n";
+$tables = $pdo->query("SHOW TABLES")->fetchAll(PDO::FETCH_COLUMN);
+foreach ($tables as $table) {
+    echo "$table\n";
+}
+
+echo "\n--- USERS SCHEMA ---\n";
+$columns = $pdo->query("DESC users")->fetchAll();
+foreach ($columns as $col) {
+    echo "{$col['Field']} - {$col['Type']} - {$col['Null']} - {$col['Key']} - {$col['Default']} - {$col['Extra']}\n";
+}
 ?>
